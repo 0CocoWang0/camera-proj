a2832bc8ab1e1769db6b92e505a2187b4c21f200
Magico <keming_wang@outlook.com>
2026-08-15 21:58:33 -0700

    tried sound effect when capture / closing

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 import cv2;
 import time;
 from pathlib import Path;
+import subprocess;
 
 # connected to iphone camera
 cap = cv2.VideoCapture(1)
-saveTo = Path(__file__).parent/"captures"
+saveTo = Path(__file__).parent/"captures1.0"
+saveTo.mkdir(exist_ok=True)
+
+
 while True:
     ret, frame = cap.read()
     #ret = return value. did this operation actually work? 
@@ -16,9 +20,11 @@ while True:
     key = cv2.waitKey(1) & 0xFF
     # ord getting numeric value
     if key == ord('q'):
+        subprocess.Popen(["afplay", "/System/Library/Sounds/Hero.aiff"])
         print("pressed q")
         break
     elif key == ord(' '):
+        subprocess.Popen(["afplay", "/System/Library/Sounds/Purr.aiff"])
         print("pressed space bar")
         shot = cv2.imwrite(f"{saveTo/time.strftime("%Y-%m-%d-%H%M%S", time.localtime())}.png",frame)
         print(str(shot))
