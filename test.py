e9a10f0cbe1e215b88b7dba0d4f57bc74b6977d8
Magico <keming_wang@outlook.com>
2026-08-16 22:15:43 -0700

    tried img filters

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from pathlib import Path;
 import subprocess;
 
 # connected to iphone camera
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 saveTo = Path(__file__).parent/"captures1.0"
 saveTo.mkdir(exist_ok=True)
 
@@ -14,8 +14,11 @@ while True:
     #ret = return value. did this operation actually work? 
     if not ret:
         break
-    cv2.imshow("frameeeeeeeeee plez", frame)
     
+    sth = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow("frameeeeeeeeee plez", frame)
+    cv2.imshow("sth", sth)
     # one waitkey call. opens the envolpe and take whatever is inside and trim it
     key = cv2.waitKey(1) & 0xFF
     # ord getting numeric value
@@ -26,8 +29,10 @@ while True:
     elif key == ord(' '):
         subprocess.Popen(["afplay", "/System/Library/Sounds/Purr.aiff"])
         print("pressed space bar")
-        shot = cv2.imwrite(f"{saveTo/time.strftime("%Y-%m-%d-%H%M%S", time.localtime())}.png",frame)
-        print(str(shot))
+        shot = cv2.imwrite(saveTo / f"{time.strftime("%Y-%m-%d-%H%M%S", time.localtime())}.png",frame)
+        shot2 = cv2.imwrite(saveTo / f"{time.strftime("%Y-%m%d-%H%M%S", time.localtime())} special.png", sth)
+        print(str(shot), str(shot2))
+    
         
 cap.release() # release the camera
 cv2.destroyAllWindows() # destroy all windows
